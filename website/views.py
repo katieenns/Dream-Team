47a7c1f01e85552dc3388c13584da02fbe47dc09
katieenns <enns.katie@gmail.com>
2022-10-21 09:52:16 -0700

    base.html ../../main.py ../auth.py ../views.py
    :wq
    
    :wq

--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=["GET", "POST"])
 def home():
+    data = request.form
+    print(data)
     return render_template("home.html")
